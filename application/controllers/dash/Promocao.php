2901716e5c850efad7687d09b48893dd6fc5c232
Mateus Medeiros <mateusmedeiros252525@hotmail.com>
2018-10-04 09:33:39 -0300

    Adicionando interface para adicionar uma nova promoção

--- application/controllers/dash/Promocao.php
+++ application/controllers/dash/Promocao.php
@@ -8,13 +8,26 @@ class Promocao extends CI_Controller {
 	/* primeira função que é chamada (carregando a tela) */
 	public function index(){
 
+        /* verifica se o adm está logado */
+		if(!$this->session->has_userdata("adm")) redirect("/");
+
         /* dados que serão passados como parâmetro */
         /* enviando como parâmetro a cor da ul */
         $data['cor_ul_gpromocoes'] = 'ul-marcada';
 
+        /* carregando as bebidas */
+        $this->load->model("dash/bebida_model", "bebida");
+        $dados['bebidas'] = $this->bebida->getBebidas();
+
         /* Carregando a view da tela de gerenciamento de promoções */
         $this->load->view("dash/base.php", $data);
-        $this->load->view("dash/gerenciar_promocoes.php");
+        $this->load->view("dash/gerenciar_promocoes.php", $dados);
 		
+    }
+
+    /* função para chamar o model e editar ou adicionar uma nova promoção */
+    public function gravar(){
+        /* verifica se o adm está logado */
+		if(!$this->session->has_userdata("adm")) redirect("/");
     }
 }
--- application/views/dash/gerenciar_promocoes.php
+++ application/views/dash/gerenciar_promocoes.php
@@ -1,7 +1,40 @@
 <?= $this->session->flashdata('gravar_dados_promocoes');?>
-
 <div>
-    <button  class = "btn btn-adicionar"><i class = " icon-espaco fa fa-plus"></i>Adicionar nova promoção</button>
+    <button data-toggle = "collapse" data-target = "#collapse-add-promocao"  class = "btn btn-adicionar"><i class = " icon-espaco fa fa-plus"></i>Adicionar nova promoção</button>
+</div>
+
+<div class = "collapse" id = "collapse-add-promocao">
+    <form method = "POST" action = "/beer-ecommerce/dash/promocao/gravar">
+        <div class = "row">
+            <div class = "col-md-4">
+                <label>Apelido para promoção</label>
+                <input class = "form-control" placeholder = "Ex: Queima total de estoque" required>
+            </div>
+
+            <div class = "col-md-4">
+                <label>Desconto aplicado (em porcentagem)</label>
+                <input class = "form-control" type = "number" step="0.01" required>
+            </div>
+
+            <div class = "col-md-4">
+                <label>O desconto será aplicado em:</label>
+                <select multiple='' name='bebidas_desconto[]' class='ui fluid normal dropdown' id = 'bebidas_desconto' required>
+                    <option value=''>Todas bebidas</option>
+                    <?php
+                        /* listando as bebidas */
+                        foreach($bebidas as $bebida)
+                            echo "<option value = '".$bebida['id_tipo_bebida']."'>".$bebida['nome_tipo_bebida']."</option>";
+
+                    ?>
+                </select>
+            </div>
+        </div>
+
+        <div class = "center div-promocoes">
+            <button type = "submit" class = "btn btn-adicionar">Gravar</button>
+        </div>
+
+    </form>
 </div>
 
 <div class = "div-promocoes">
