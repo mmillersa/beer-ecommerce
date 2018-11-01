60d9ff1bf1bd2d3bb1fef4395d84b1995a444ea3
eduviictor <eduvictornobrega@gmail.com>
2018-11-01 02:22:22 -0300

    Preparando paginas login e mail

---
+++ application/controllers/public/Mail.php
@@ -0,0 +1,15 @@
+<?php 
+
+/* Não permitindo que a URL seja acessada diretamente */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+Class Mail extends CI_Controller{
+
+    public function index(){
+        $this->load->view("public/mail.php");
+    }
+
+}
+
+
+?>
