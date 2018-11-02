a13d4a55d7bab8bde2e549cb132fd56e5c72fc0d
eduviictor <eduvictornobrega@gmail.com>
2018-11-01 23:17:52 -0300

    Adicionando header e footer dinamico as paginas household e kitchen

--- application/controllers/public/Kitchen.php
+++ application/controllers/public/Kitchen.php
@@ -6,7 +6,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 Class Kitchen extends CI_Controller{
 
     public function index(){
+        $this->load->view('public/header.php');
         $this->load->view("public/kitchen.php");
+        $this->load->view('public/footer.php');
     }
 
 }
