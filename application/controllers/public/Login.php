d29d387cb20a214bb69ea10249c7a1e32dc70616
eduviictor <eduvictornobrega@gmail.com>
2018-11-01 23:29:50 -0300

    Adicionando header e footer dinamico as paginas login e mail

--- application/controllers/public/Login.php
+++ application/controllers/public/Login.php
@@ -6,7 +6,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 Class Login extends CI_Controller{
 
     public function index(){
+        $this->load->view('public/header.php');
         $this->load->view("public/login.php");
+        $this->load->view('public/footer.php');
     }
 
 }
--- application/controllers/public/Mail.php
+++ application/controllers/public/Mail.php
@@ -6,7 +6,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 Class Mail extends CI_Controller{
 
     public function index(){
+        $this->load->view('public/header.php');
         $this->load->view("public/mail.php");
+        $this->load->view('public/footer.php');
     }
 
 }
