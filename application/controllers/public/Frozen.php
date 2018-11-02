905427b6e66d86ffcf9a570555e51e69a252c29b
eduviictor <eduvictornobrega@gmail.com>
2018-11-01 23:14:08 -0300

    Adicionando header e footer dinamico as paginas frozen e home

--- application/controllers/public/Frozen.php
+++ application/controllers/public/Frozen.php
@@ -6,7 +6,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 Class Frozen extends CI_Controller{
 
     public function index(){
+        $this->load->view('public/header.php');
         $this->load->view("public/frozen.php");
+        $this->load->view('public/footer.php');
     }
 
 }
--- application/controllers/public/Home.php
+++ application/controllers/public/Home.php
@@ -6,7 +6,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 Class Home extends CI_Controller{
 
     public function index(){
+        $this->load->view('public/header.php');
         $this->load->view("public/home.php");
+        $this->load->view('public/footer.php');
     }
 
 }
